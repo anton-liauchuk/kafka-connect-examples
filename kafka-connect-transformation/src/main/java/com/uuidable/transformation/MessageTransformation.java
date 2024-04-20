b490d6c86ef82f24b7c5652325763179e9c08b9f
Anton Liauchuk <anton93lev@gmail.com>
2024-04-20 18:33:00 +0300

    message transformation

--- kafka-connect-transformation/src/main/java/com/uuidable/transformation/MessageTransformation.java
+++ kafka-connect-transformation/src/main/java/com/uuidable/transformation/MessageTransformation.java
@@ -6,44 +6,54 @@ import java.util.UUID;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.Requirements;
 
 public class MessageTransformation<R extends ConnectRecord<R>> implements Transformation<R> {
 
+	private static final String ID_FIELD = "id";
+	private static final String NAME = "name";
 	private static final String PURPOSE = "message model";
 	private static final String NAME_DEFAULT_VALUE = "default_name";
-	private static final String FIELD_NAME_KEY = "name";
+	private static final String PAGEID_KEY = "pageid";
 
 	@Override
 	public R apply(R record) {
+		var transformedSchema = SchemaBuilder.struct()
+				.field(ID_FIELD, Schema.STRING_SCHEMA)
+				.field(NAME, Schema.STRING_SCHEMA)
+				.build();
+
 		Schema schema = record.valueSchema();
 		if (schema == null) {
-			var message = new Message();
-			message.setId(UUID.randomUUID());
-			message.setName(NAME_DEFAULT_VALUE);
+			var transformed = new Struct(transformedSchema)
+					.put(ID_FIELD, UUID.randomUUID().toString())
+					.put(NAME, NAME_DEFAULT_VALUE);
+
 			return record.newRecord(
 					record.topic(),
 					record.kafkaPartition(),
-					record.keySchema(),
-					message.getId(),
-					null,
-					message,
+					Schema.STRING_SCHEMA,
+					transformed.get(ID_FIELD),
+					transformedSchema,
+					transformed,
 					record.timestamp()
 			);
 		} else {
 			var value = Requirements.requireStruct(record.value(), PURPOSE);
-			var message = new Message();
-			message.setId(UUID.randomUUID());
-			message.setName(value.get(FIELD_NAME_KEY).toString());
+			var transformed = new Struct(transformedSchema)
+					.put(ID_FIELD, UUID.randomUUID().toString())
+					.put(NAME, value.get(PAGEID_KEY).toString());
 
 			return record.newRecord(
 					record.topic(),
 					record.kafkaPartition(),
-					record.keySchema(),
-					message.getId(),
-					null,
-					message,
+					Schema.STRING_SCHEMA,
+					transformed.get(ID_FIELD),
+					transformedSchema,
+					transformed,
 					record.timestamp()
 			);
 		}
@@ -51,7 +61,7 @@ public class MessageTransformation<R extends ConnectRecord<R>> implements Transf
 
 	@Override
 	public ConfigDef config() {
-		return null;
+		return new ConfigDef();
 	}
 
 	@Override
